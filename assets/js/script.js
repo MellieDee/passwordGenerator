1d9d11230f5fee7b095312317f6b0372604080b7
Mellie Dee <89175650+MellieDee@users.noreply.github.com>
2021-10-21 11:31:09 -0700

    pseudocode/outline js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,63 @@
 // Assignment code here
 //to generate a random number
-var  pwLength = parseInt(window.prompt("Let's create your new password. How long do you want it to be? Pick a number between 8 and 128."))
-  console.log(pwLength);
 
-// pwLength()
+// ******** DATA GENERATION   **********
+
+// (1)  ********  LENGTH & VALIDATE prompt ********
+//a) validate input is filled
+  //if is empty send reminder to fill
+//(b) input = number 
+  // validate input is a number
+  //if  not a number, send reminder to input a number
+  // put in place safeguard if number is string
+//(c) validate number is 8 < x < 128
+  //if number is less or more, send reminder to pick between 8/128
+
+//(2)  ********* LOWERCASE Prompt *******
+  //(a) validate entered yes or no -
+        //set safeguard for case or just letter
+  //(b) if not y or n then send alert to pick valid answer
+
+//(3) ********* UPPERCASE Prompt *******
+  //(a) validate entered yes or no -
+        //set safeguard for case or just letter
+  //(b) if not y or n then send alert to pick valid answer
+
+// (4) ********* NUMERIC Prompt *******
+  //(a) validate entered yes or no -
+        //set safeguard for case or just letter
+  //(b) if not y or n then send alert to pick valid answer
+
+  // (5) *********  SPECIAL Prompt *******
+   //(a) validate entered yes or no -
+        //set safeguard for case or just letter
+  //(b) if not y or n then send alert to pick valid answer
+
+
+//*********  CREATE RANDOM NUMBER FUNCITON  ********/
+
+
+
+//********** CREATE FUNCTION TO DISPLAY NEW PW ********/
+
+
+
+
+var  pwLength = parseInt(window.prompt("Let's create your new password. How long do you want it to be? Pick a number between 8 and 128."))
+console.log(pwLength + " characters long.");
+
+
+
+var charLower = window.prompt("Should it have lowercase letters? Yes or No?");console.log(charLower + " incl lower");
+
+var charUpper = window.prompt("Should it have UPPERCASE letters? Yes or No?")
+console.log(charUpper + " incl Upper");
+
+var charNumeric = window.prompt("Should it have numbers? Yes or No?");
+console.log(charNumeric + " incl numbers");
+
+var charSpecial = window.prompt("Should it special characters, like !,&,%, %, # or *? Yes or No?");
+console.log(charSpecial + " incl $p*c!@l");
 
 
 // document.getElementById("password").innerHTML = "Your new password is " + Math.random() +  ".";
@@ -15,6 +69,8 @@ var  pwLength = parseInt(window.prompt("Let's create your new password. How long
 //     console.log(value);
 // };
 
+
+////*******GIVEN CODE****** */
 // // Get references to the #generate element
 // var generateBtn = document.querySelector("#generate");
 
